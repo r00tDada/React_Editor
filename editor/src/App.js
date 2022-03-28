698a815b4d61a147cf87374b79d0d9a55579e70e
r00tDada <sourabh18299@gmail.com>
2022-03-28 17:32:34 +0530

    first commit

---
+++ editor/src/App.js
@@ -0,0 +1,11 @@
+import Editor from "./components/";
+
+function App() {
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
---
+++ editor/src/components/Editor.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Plate } from "@udecode/plate";
+
+function Editor() {
+  return (
+    <>
+      <div>Editor</div>
+      <Plate />
+    </>
+  );
+}
+
+export default Editor;
