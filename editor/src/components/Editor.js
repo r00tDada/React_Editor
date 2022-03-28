254a34ee57698fdb5b23a527be366b9cc9855fba
r00tDada <sourabh18299@gmail.com>
2022-03-28 17:47:41 +0530

    added sample

--- editor/src/components/Editor.js
+++ editor/src/components/Editor.js
@@ -2,14 +2,26 @@ import React from "react";
 import { Plate } from "@udecode/plate";
 
 function Editor() {
+  // Stored in VALUES.plainText
   const editableProps = {
     placeholder: "Type…",
     style: {
       padding: "15px",
     },
   };
+  const initialValue = [
+    {
+      children: [
+        {
+          text: "This is editable plain text with react and history plugins, just like a <textarea>!",
+        },
+      ],
+    },
+  ];
 
-  return <Plate id="1" editableProps={editableProps} />;
+  return (
+    <Plate id="2" editableProps={editableProps} initialValue={initialValue} />
+  );
 }
 
 export default Editor;
